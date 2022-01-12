89d0c55a69fc56da47ea65ab9559ddb3aa9ec4ef
abhip07 <65076876+abhip07@users.noreply.github.com>
2022-01-12 12:39:39 +0530

    added code for half adder

---
+++ half_adder/half_adder.v
@@ -0,0 +1,18 @@
+// Half Adder in Verilog HDL
+module half_adder
+  (
+   a,
+   b,
+   sum,
+   carry
+   );
+
+  input  a;
+  input  b;
+  output sum;
+  output carry;
+
+  assign sum   = a ^ b;  // bitwise xor
+  assign carry = a & b;  // bitwise and
+
+endmodule // half_adder
